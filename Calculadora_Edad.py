175b189e5582d019460e31a523b9608bff745b06
SantiagoRey <sjcenteno@uamv.edu.ni>
2026-08-31 10:40:51 -0600

    actualizo el ejercicio

--- Calculadora_Edad.py
+++ Calculadora_Edad.py
@@ -1,7 +1,13 @@
-try:
-    edad = int(input("Edad: "))
-    print("Edad registrada:", edad)
-except ValueError:
-    print("Debe ingresar un número entero.")
-finally:
-    print("Proceso finalizado.")
+from colorama import Fore, init
+
+init(autoreset=True)
+
+while True:
+    try:
+        dad = int(input("Edad: "))
+        print("Edad registrada:", edad)
+        break 
+    except ValueError:
+     print(Fore.RED + "Debe ingresar un número entero.")
+    finally:
+     print("Proceso finalizado.")
